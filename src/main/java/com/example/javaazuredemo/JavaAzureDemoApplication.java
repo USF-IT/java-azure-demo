04f01e84f7511c2f33c8a314736e4a5b997ebbae
Anusha Ande <36480151+anushaande@users.noreply.github.com>
2024-01-22 10:38:17 -0500

    initial commit

---
+++ src/main/java/com/example/javaazuredemo/JavaAzureDemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.javaazuredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JavaAzureDemoApplication {
+
+    @GetMapping("/message")
+    public String message(){
+        return "Congrats! Your app is deployed successfully in Azure!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaAzureDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javaazuredemo/JavaAzureDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaazuredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAzureDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
