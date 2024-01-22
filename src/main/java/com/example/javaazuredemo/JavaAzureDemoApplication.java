a036d4f575378c2eee51112723c9ac6fd957059f
Anusha Ande <36480151+anushaande@users.noreply.github.com>
2024-01-22 10:49:38 -0500

    Second commit

--- src/main/java/com/example/javaazuredemo/JavaAzureDemoApplication.java
+++ src/main/java/com/example/javaazuredemo/JavaAzureDemoApplication.java
@@ -11,7 +11,7 @@ public class JavaAzureDemoApplication {
 
     @GetMapping("/message")
     public String message(){
-        return "Congrats! Your app is deployed successfully in Azure!";
+        return "Congrats! Your app is deployed successfully in Azure! Application is redeployed";
     }
 
     public static void main(String[] args) {
